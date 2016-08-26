e794b801c6fa7ee3dcfd31f523da3e7de961d3f4
yaojm0125 <yjmAndroid@163.com>
2016-08-26 14:29:26 +0800

    修改Gradle配置,删除main下jni里面的Hello.c文件,配置sourceSets.main

--- app/src/main/java/com/example/yaojm/ndkdemo/MainActivity.java
+++ app/src/main/java/com/example/yaojm/ndkdemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.yaojm.ndkdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.tv);
+        tv.setText(sayHello());
+
+    }
+
+    public native String sayHello();
+
+    static {
+        System.loadLibrary("hello");
     }
 }
--- app/src/main/jni/hello.c
+++
@@ -1,8 +0,0 @@
-#include <String.h>
-#include <jni.h>
-
-jstring com_example_yaojm_ndkdemo_MainActivity_sayHello(JNIEnv *env,jobject thiz){
-
-    return (*env)->NewStringUTF(env,"Hello from JNI !");
-
-}
